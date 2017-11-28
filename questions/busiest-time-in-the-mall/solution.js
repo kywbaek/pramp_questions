823523c50abf8c6b1d6d32c0cbb6043d43efe86c
Young Wook Baek <kywbaek@gmail.com>
2017-11-28 01:36:29 +0000

    docs: Convert pseudocode to code for Busiest..

--- questions/busiest-time-in-the-mall/solution.js
+++ questions/busiest-time-in-the-mall/solution.js
@@ -1,27 +1,41 @@
-// Pseudocode
-function findBusiestPeriod(data):
-    n = data.length
-    count = 0
-    maxCount = 0
-    maxPeriodTime = 0
+function findBusiestPeriod(data) {
+  let n = data.length
+  let count = 0
+  let maxCount = 0
+  let maxPeriodTime = 0
 
-    for i from 0 to n-1:
-        // update count
-        if (data[i][2] == 1): // visitors entered the mall
-            count += data[i][1]
-        else if (data[i][2] == 0): // visitors existed the mall
-            count -= data[i][1]
+  for (let i=0;i<n;i++) {
+    if (data[i][2] === 1) {
+      count += data[i][1];
+    }
+    else if (data[i][2] === 0) {
+      count -= data[i][1];
+    }
 
-        // check for other data points with the same timestamp
-        if (i < n-1 AND data[i][0] == data[i+1][0]):
-            continue
+    if (i < n-1 && data[i][0] === data[i+1][0]) {
+      continue
+    }
 
-        // update maximum
-        if (count > maxCount):
-            maxCount = count
-            maxPeriodTime = data[i][0]
+    if (count > maxCount) {
+      maxCount = count
+      maxPeriodTime = data[i][0]
+    }
+  }
 
-    return maxPeriodTime
+  return maxPeriodTime
+}
+
+const data = [ [1487799425, 14, 1],
+               [1487799425, 4,  0],
+               [1487799425, 2,  0],
+               [1487800378, 10, 1],
+               [1487801478, 18, 0],
+               [1487801478, 18, 1],
+               [1487901013, 1,  0],
+               [1487901211, 7,  1],
+               [1487901211, 7,  0] ];
+
+console.log(findBusiestPeriod(data));
 
 /*
 Time Complexity: O(N) since every data point in the input array gets visited only once in the for loop.
