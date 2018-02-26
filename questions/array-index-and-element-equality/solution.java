5ae696fe3145ace321f0dadee7f5dd8951501322
Young Wook Baek <kywbaek@gmail.com>
2018-02-26 02:48:32 +0000

    docs: add solution.java to Array Index..

---
+++ questions/array-index-and-element-equality/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+  static int indexEqualsValueSearch(int[] arr) {
+    int lo=0;
+    int hi=arr.length - 1;
+
+    while (lo<=hi) {
+      int md= (int) ((lo+hi)/2);
+      if (arr[md]<md) {
+        lo = md + 1;
+      } else if (arr[md]==md && (md==0 || arr[md-1]<(md-1))) {
+        return md;
+      } else {
+        hi = md - 1;
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+  }
+}
