88de1b0f0d746df81c763225cf746296e5eba835
Young Wook Baek <kywbaek@gmail.com>
2018-03-17 03:32:16 +0100

    fix: replace with more efficient solutions on Getting..

--- questions/getting-a-different-number/solution.py
+++ questions/getting-a-different-number/solution.py
@@ -1,26 +1,19 @@
-# time/space - O(n)
-def get_different_number1(arr):
-  mySet = set(arr)
-  for i in xrange(len(arr)):
-    if i not in mySet:
-      return i
-  return len(arr)
-
-# time - O(n), space - O(1)
-
-
+# time - O(n*logn), space - O(1)
 def get_different_number(arr):
+  arr.sort()
   n = len(arr)
-  for i in xrange(n):
-    temp = arr[i]
-    while (temp < n and arr[temp] != temp):
-      temp1 = arr[temp]
-      arr[temp] = temp
-      temp = temp1
   for i in xrange(n):
     if arr[i] != i:
       return i
   return n
 
-
-print get_different_number1([1, 3, 0, 2])
+# time - O(n), space - O(n)
+def get_different_number1(arr):
+  n = len(arr)
+  dic = {}
+  for i in xrange(n):
+    dic[arr[i]] = True
+  for i in xrange(n):
+    if i not in dic:
+      return i
+  return n
