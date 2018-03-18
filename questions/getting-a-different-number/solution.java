966f680b6fcfcf1ed4786c79d9608e2b1efc2759
Young Wook Baek <kywbaek@gmail.com>
2018-03-18 14:22:07 -0400

    docs: add solution.java to Getting A Different..

---
+++ questions/getting-a-different-number/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+  static int getDifferentNumber(int[] arr) {
+    int n=arr.length;
+    Map<Integer,Boolean> map = new HashMap<Integer,Boolean>();
+    for (int i=0;i<n;i++) {
+      map.put(arr[i],true);
+    }
+    for (int i=0;i<n;i++) {
+      if (map.get(i)==null) {
+        return i;
+      }
+    }
+    return n;
+  }
+
+  public static void main(String[] args) {
+  }
+}
