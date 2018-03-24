a7c20a795bde40dd5f92f717e700435dcaa53298
Young Wook Baek <kywbaek@gmail.com>
2018-03-24 04:01:49 +0100

    docs: add solution using heap to K-Messed..

--- questions/k-messed-array-sort/solution.py
+++ questions/k-messed-array-sort/solution.py
@@ -1,4 +1,21 @@
-def k_selection_sort(arr, k):
+import heapq
+# time: O(N logK)   space: O(K)
+def sort_k_messed_array(arr, k):
+    n = len(arr)
+    tempArr = arr[:k + 1]
+    heapq.heapify(tempArr)
+
+    for i in xrange(k + 1, n):
+        arr[i - (k + 1)] = heapq.heappop(tempArr)
+        heapq.heappush(tempArr, arr[i])
+
+    for i in xrange(k + 1):
+        arr[n - k - 1 + i] = heapq.heappop(tempArr)
+
+    return arr
+
+# k selection sort
+def sort_k_messed_array1(arr, k):
     n = len(arr)
     for i in xrange(n):
         Index = i
@@ -14,4 +31,4 @@ def k_selection_sort(arr, k):
 arr = [1, 4, 5, 2, 3, 7, 8, 6, 10, 9]
 k = 2
 
-print k_selection_sort(arr, k)
+print sort_k_messed_array(arr, k)
