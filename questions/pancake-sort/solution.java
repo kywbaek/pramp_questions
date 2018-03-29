ee0fe669388e8f0136bc8dc623781d261e86dad1
Young Wook Baek <kywbaek@gmail.com>
2018-03-29 03:15:52 +0100

    docs: add solution.java to Pancake Sort

---
+++ questions/pancake-sort/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+  static void flip(int[] arr, int k) {
+    int lo=0, hi=k-1;
+    while (lo<hi) {
+      int temp = arr[hi];
+      arr[hi] = arr[lo];
+      arr[lo] = temp;
+      lo++;
+      hi--;
+    }
+  }
+
+  static int getMaxIndex(int[] arr, int k) {
+    int maxE = arr[0];
+    int maxI = 0;
+    for (int i=1;i<k;i++) {
+      if (arr[i]>maxE) {
+        maxE = arr[i];
+        maxI = i;
+      }
+    }
+    return maxI;
+  }
+
+  static int[] pancakeSort(int[] arr) {
+    int maxInd;
+    for (int i=arr.length;i>1;i--) {
+      maxInd = getMaxIndex(arr,i);
+      flip(arr, maxInd+1);
+      flip(arr, i);
+    }
+    return arr;
+  }
+
+  public static void main(String[] args) {
+  }
+}
