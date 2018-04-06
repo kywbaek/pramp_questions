ce911e22e2e788e572f088972138cc589173b421
Young Wook Baek <kywbaek@gmail.com>
2018-04-07 00:05:24 +0100

    docs: add solution.java to Time Planner

---
+++ questions/time-planner/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+
+  static int[] meetingPlanner(int[][] slotsA, int[][] slotsB, int dur) {
+    int ia=0, ib=0;
+    int start, end;
+    while (ia<slotsA.length && ib<slotsB.length) {
+      start = Math.max(slotsA[ia][0], slotsB[ib][0]);
+      end = Math.min(slotsA[ia][1], slotsB[ib][1]);
+
+      if (start + dur <= end) {
+        return new int[] {start, start+dur};
+      }
+
+      if (slotsA[ia][1] < slotsB[ib][1]) {
+        ia++;
+      } else {
+        ib++;
+      }
+    }
+    return new int[] {};
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
