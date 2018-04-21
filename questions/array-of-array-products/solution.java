9af43a8190df2c34fd57d0b28d55e3e34bfbed75
Young Wook Baek <kywbaek@gmail.com>
2018-04-21 02:44:16 +0100

    feat: add example cases to Array of Array..

--- questions/array-of-array-products/solution.java
+++ questions/array-of-array-products/solution.java
@@ -1,5 +1,4 @@
 class Solution {
-
   static int[] arrayOfArrayProducts(int[] arr) {
     if (arr.length<=1) {
       return new int[0];
@@ -20,5 +19,12 @@ class Solution {
   }
 
   public static void main(String[] args) {
+    int[] arr1 = [8, 10, 2];
+    // Expected output: [20, 16, 80] by calculating [10*2, 8*2, 8*10]
+    System.out.println(arrayOfArrayProducts(arr1));
+
+    int[] arr2 = [2, 7, 3, 4];
+    // Expected output: [84, 24, 56, 42] by calculating [7*3*4, 2*3*4, 2*7*4, 2*7*3]
+    System.out.println(arrayOfArrayProducts(arr2));
   }
 }
