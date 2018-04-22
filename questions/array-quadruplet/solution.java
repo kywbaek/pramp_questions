1ab0c6d3914b23106715949ad2a432b2d0f74453
Young Wook Baek <kywbaek@gmail.com>
2018-04-23 00:50:07 +0100

    fix: add necessary argument to example case func

--- questions/array-quadruplet/solution.java
+++ questions/array-quadruplet/solution.java
@@ -35,6 +35,6 @@ class Solution {
     /*  The ordered quadruplet of (7, 4, 0, 9) whose sum is 20. Notice that there
        are two other quadruplets whose sum is 20: (7, 9, 1, 3) and (2, 4, 9, 5),
        but again you’re asked to return the just one quadruplet (in an ascending order) */
-    System.out.println(findArrayQuadruplet(arr));
+    System.out.println(findArrayQuadruplet(arr, s));
   }
 }
