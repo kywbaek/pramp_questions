548b512df83b4549ed999d502500ce76a06114ed
Young Wook Baek <kywbaek@gmail.com>
2018-04-22 03:05:56 +0100

    feat: add example cases to Array Quadruplet

--- questions/array-quadruplet/solution.java
+++ questions/array-quadruplet/solution.java
@@ -1,5 +1,4 @@
 class Solution {
-
   static int[] findArrayQuadruplet(int[] arr, int s) {
     int n = arr.length;
     int[] emptyArr = new int[0];
@@ -29,5 +28,13 @@ class Solution {
   }
 
   public static void main(String[] args) {
+    int [] arr = [2, 7, 4, 0, 9, 5, 1, 3];
+    int s = 20;
+
+    // Expected output: [0, 4, 7, 9]
+    /*  The ordered quadruplet of (7, 4, 0, 9) whose sum is 20. Notice that there
+       are two other quadruplets whose sum is 20: (7, 9, 1, 3) and (2, 4, 9, 5),
+       but again you’re asked to return the just one quadruplet (in an ascending order) */
+    System.out.println(findArrayQuadruplet(arr));
   }
 }
