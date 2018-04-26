e2d304ff0c47b4c816c204019045825de5af6a54
Young Wook Baek <kywbaek@gmail.com>
2018-04-26 01:54:00 +0100

    feat: add example case to Busiest Time...

--- questions/busiest-time-in-the-mall/solution.java
+++ questions/busiest-time-in-the-mall/solution.java
@@ -1,5 +1,4 @@
 class Solution {
-
   static int findBusiestPeriod(int[][] data) {
     int maxV = 0, curV = 0, time = 0, n = data.length;
     for (int i=0;i<n;i++) {
@@ -19,5 +18,17 @@ class Solution {
   }
 
   public static void main(String[] args) {
+    int [][] data = [ [1487799425, 14, 1],
+                      [1487799425, 4,  0],
+                      [1487799425, 2,  0],
+                      [1487800378, 10, 1],
+                      [1487801478, 18, 0],
+                      [1487801478, 18, 1],
+                      [1487901013, 1,  0],
+                      [1487901211, 7,  1],
+                      [1487901211, 7,  0] ];
+    // Expected output: 1487800378
+    // since the increase in the number of people in the mall is the highest at that point
+    System.out.println(findBusiestPeriod(data));
   }
 }
