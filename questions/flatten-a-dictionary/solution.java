8eda484e4a7b7e4b116e74553d37bf3cdaea8723
Young Wook Baek <kywbaek@gmail.com>
2018-05-01 18:03:52 +0100

    feat: add example case to Flatten..

--- questions/flatten-a-dictionary/solution.java
+++ questions/flatten-a-dictionary/solution.java
@@ -5,12 +5,13 @@ class Solution {
     HashMap x = (HashMap<Integer, String>)o;
     return x;
    }
-    static HashMap<String, String> flattenDictionary(HashMap<String, Object> dict) {
+
+  static HashMap<String, String> flattenDictionary(HashMap<String, Object> dict) {
     HashMap<String, String> flatDict = new HashMap<String, String>();
     helper("", dict, flatDict);
-
     return flatDict;
   }
+
   static void helper(String initialKey, HashMap<String,Object> dict, HashMap<String,String> flatDict) {
     for (String key: dict.keySet()) {
       Object value = dict.get(key);
@@ -31,5 +32,23 @@ class Solution {
   }
 
   public static void main(String[] args) {
+    /*
+    Input: dict = {"Key1" : "1",
+                   "Key2" : {"a" : "2",
+                             "b" : "3",
+                             "c" : {"d" : "3",
+                                    "e" : {"" : "1"}
+                                   }
+                            }
+                  }
+
+    Expected Output: {
+            "Key1" : "1",
+            "Key2.a" : "2",
+            "Key2.b" : "3",
+            "Key2.c.d" : "3",
+            "Key2.c.e" : "1"
+                     }
+    */
   }
 }
