98c8a3b7a0a64ccc4ce03b26384038f486a3c26a
Young Wook Baek <kywbaek@gmail.com>
2018-06-06 00:58:06 +0100

    feat: add example case to Martix Spiral...

--- questions/matrix-spiral-copy/solution.java
+++ questions/matrix-spiral-copy/solution.java
@@ -39,5 +39,14 @@ class Solution {
   }
 
   public static void main(String[] args) {
+    int[][] inputMatrix  = {{1, 2, 3, 4, 5},
+                           {6, 7, 8, 9, 10},
+                           {11, 12, 13, 14, 15},
+                           {16, 17, 18, 19, 20}};
+    // Expected output: {1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12}
+    int [] output = spiralCopy(inputMatrix);
+    for (int i:output) {
+      System.out.print(i+" ");
+    }
   }
 }
