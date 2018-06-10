150181f53458088c515fa7860bb8a827e4b7997d
Young Wook Baek <kywbaek@gmail.com>
2018-06-11 00:05:10 +0100

    fix: array syntax on Busiest Time...

--- questions/busiest-time-in-the-mall/solution.java
+++ questions/busiest-time-in-the-mall/solution.java
@@ -18,15 +18,15 @@ class Solution {
   }
 
   public static void main(String[] args) {
-    int [][] data = [ [1487799425, 14, 1],
-                      [1487799425, 4,  0],
-                      [1487799425, 2,  0],
-                      [1487800378, 10, 1],
-                      [1487801478, 18, 0],
-                      [1487801478, 18, 1],
-                      [1487901013, 1,  0],
-                      [1487901211, 7,  1],
-                      [1487901211, 7,  0] ];
+    int [][] data = { {487799425,  14, 1},
+                      {1487799425, 4,  0},
+                      {1487799425, 2,  0},
+                      {1487800378, 10, 1},
+                      {1487801478, 18, 0},
+                      {1487801478, 18, 1},
+                      {1487901013, 1,  0},
+                      {1487901211, 7,  1},
+                      {1487901211, 7,  0} };
     // Expected output: 1487800378
     // since the increase in the number of people in the mall is the highest at that point
     System.out.println(findBusiestPeriod(data));
