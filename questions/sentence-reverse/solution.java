6c5722d0370b6b0e2311a08667ef5f1e9ff2b226
Young Wook Baek <kywbaek@gmail.com>
2018-06-19 02:12:05 +0100

    feat: add example case to Sentence Reverse

--- questions/sentence-reverse/solution.java
+++ questions/sentence-reverse/solution.java
@@ -25,5 +25,16 @@ class Solution {
   }
 
   public static void main(String[] args) {
+    char[] arr = { 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',
+                'm', 'a', 'k', 'e', 's', '  ',
+                'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' };
+    /* Expected output: [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ',
+                          'm', 'a', 'k', 'e', 's', '  ',
+                          'p', 'e', 'r', 'f', 'e', 'c', 't' ] */
+    char[] output = reverseWords(arr);
+    for (char c: output) {
+      System.out.print(c);
+      System.out.print(" ");
+    }
   }
 }
