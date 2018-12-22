d3710a31d65976f31b526526c51db3993eed262a
Young <kywbaek@gmail.com>
2018-12-22 12:22:02 -0500

    feat: add example case

--- questions/award-budget-cuts/solution.py
+++ questions/award-budget-cuts/solution.py
@@ -8,6 +8,17 @@ def find_grants_cap(grantsArray, newBudget):
     return grantsArray[0]
 
   for i in xrange(n):
-    surplus -= (i+1) * (grantsArray[i] - grantsArray[i+1])
+    surplus -= (i + 1) * (grantsArray[i] - grantsArray[i + 1])
     if surplus <= 0:
-      return grantsArray[i+1] - (surplus / float(i+1))
+      return grantsArray[i + 1] - (surplus / float(i + 1))
+
+
+grantsArray = [2, 100, 50, 120, 1000]
+newBudget = 190
+
+# Expected output: 47
+"""
+ and given this cap the new grants array would be [2, 47, 47, 47, 47].
+    Notice that the sum of the new grants is indeed 190
+"""
+print find_grants_cap(grantsArray, newBudget)
