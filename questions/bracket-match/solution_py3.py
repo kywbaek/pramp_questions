5f6f34df3bed0c6f950813e147d0fa9e2eba3f3f
Young <kywbaek@gmail.com>
2019-01-04 22:55:17 -0500

    feat: add solution in python3

---
+++ questions/bracket-match/solution_py3.py
@@ -0,0 +1,20 @@
+def bracket_match(text):
+  oB, cB = 0, 0
+  for chr in text:
+    if chr == '(':
+      oB += 1
+    else:
+      if oB > 0:
+        oB -= 1
+      else:
+        cB += 1
+
+  return oB + cB
+
+
+text = "(()"
+print(bracket_match(text))
+text = "(())"
+print(bracket_match(text))
+text = "())("
+print(bracket_match(text))
