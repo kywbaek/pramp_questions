6f495872ab5f6569298d887bdeb921d79c239d86
Young <kywbaek@gmail.com>
2019-01-14 21:14:49 -0500

    feat: change range to xrange

--- questions/busiest-time-in-the-mall/solution.py
+++ questions/busiest-time-in-the-mall/solution.py
@@ -3,26 +3,27 @@ def find_busiest_period(data):
   busyTime = 0
   curVisitor = 0
   maxVisitor = 0
-  for i in range(n):
+  for i in xrange(n):
     if data[i][2] == 1:
       curVisitor += data[i][1]
     elif data[i][2] == 0:
       curVisitor -= data[i][1]
-    if (i < (n - 1)) and (data[i+1][0] == data[i][0]):
+    if (i < (n - 1)) and (data[i + 1][0] == data[i][0]):
       continue
     if curVisitor > maxVisitor:
-       busyTime = data[i][0]
-       maxVisitor = curVisitor
+      busyTime = data[i][0]
+      maxVisitor = curVisitor
   return busyTime
 
-data = [ [1487799425, 14, 1],
-                 [1487799425, 4,  0],
-                 [1487799425, 2,  0],
-                 [1487800378, 10, 1],
-                 [1487801478, 18, 0],
-                 [1487801478, 18, 1],
-                 [1487901013, 1,  0],
-                 [1487901211, 7,  1],
-                 [1487901211, 7,  0] ]
+
+data = [[1487799425, 14, 1],
+        [1487799425, 4, 0],
+        [1487799425, 2, 0],
+        [1487800378, 10, 1],
+        [1487801478, 18, 0],
+        [1487801478, 18, 1],
+        [1487901013, 1, 0],
+        [1487901211, 7, 1],
+        [1487901211, 7, 0]]
 
 print find_busiest_period(data)
