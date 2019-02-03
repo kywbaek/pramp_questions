b882c828323a402ca149691304fca25022fcef80
Young <kywbaek@gmail.com>
2019-02-03 17:37:47 -0500

    feat: add example case

--- questions/drone-flight-planner/solution.js
+++ questions/drone-flight-planner/solution.js
@@ -8,6 +8,16 @@ function calcDroneMinEnergy(route) {
   return maxH - route[0][2];
 }
 
+route = [[0,   2, 10],
+        [3,   5,  0],
+        [9,  20,  6],
+        [10, 12, 15],
+        [10, 10,  8]];
+
+// Expected output: 5 #
+    /* less than 5 kWh and the drone would crash before the finish line.
+      More than `5` kWh and it’d end up with excess energy. */
+console.log(calcDroneMinEnergy(route))
 /*
 Time Complexity: O(N). We have a single loop that iterates once over route and conducts a constant number of operations at each iteration.
 
