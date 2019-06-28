92aa1ad11c7aa2af47640ac67876b8c47d2e0d8a
Young <kywbaek@gmail.com>
2019-06-27 21:28:23 -0400

    feat: add solution in python3

---
+++ questions/getting-a-different-number/solution_py3.py
@@ -0,0 +1,30 @@
+# time - O(n*logn), space - O(1)
+def get_different_number(arr):
+  arr.sort()
+  n = len(arr)
+  for i in range(n):
+    if arr[i] != i:
+      return i
+  return n
+
+# time - O(n), space - O(n)
+
+
+def get_different_number1(arr):
+  n = len(arr)
+  dic = {}
+  for i in range(n):
+    dic[arr[i]] = True
+  for i in range(n):
+    if i not in dic:
+      return i
+  return n
+
+
+arr = [0, 1, 2, 3]
+# Expected output: 4
+print(get_different_number1(arr))
+
+arr = [5, 1, 0, 3]
+# Expected output: 2
+print(get_different_number1(arr))
