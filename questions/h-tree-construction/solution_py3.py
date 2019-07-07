1095e03d8272c868d8ff70063281d4afc2dbb1b4
Young <kywbaek@gmail.com>
2019-07-07 16:11:22 -0400

    feat: add solution in python3

---
+++ questions/h-tree-construction/solution_py3.py
@@ -0,0 +1,23 @@
+def roundCoord(point):
+  return (round(point[0], 2), round(point[1], 2))
+
+
+def drawLine(x, y):
+  print(roundCoord(x), '-', roundCoord(y))
+
+
+def drawHTree(c, l, d):
+  if d == 0:
+    return
+  drawLine((c[0] - l / 2, c[1]), (c[0] + l / 2, c[1]))
+  drawLine((c[0] - l / 2, c[1] + l / 2), (c[0] - l / 2, c[1] - l / 2))
+  drawLine((c[0] + l / 2, c[1] + l / 2), (c[0] + l / 2, c[1] - l / 2))
+  print('Drew the H-tree with center at', roundCoord(c))
+
+  drawHTree((c[0] - l / 2, c[1] + l / 2), l / (2**0.5), d - 1)
+  drawHTree((c[0] - l / 2, c[1] - l / 2), l / (2**0.5), d - 1)
+  drawHTree((c[0] + l / 2, c[1] + l / 2), l / (2**0.5), d - 1)
+  drawHTree((c[0] + l / 2, c[1] - l / 2), l / (2**0.5), d - 1)
+
+
+drawHTree((0, 0), 2, 3)
