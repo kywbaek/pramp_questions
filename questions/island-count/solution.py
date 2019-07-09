78cc447d947dc53d11de7f4bf40091e3270709e2
Young <kywbaek@gmail.com>
2019-07-09 11:15:48 -0400

    feat: add another example case

--- questions/island-count/solution.py
+++ questions/island-count/solution.py
@@ -1,21 +1,22 @@
-def appendIf(q,row,col,r,c):
-  if row>=0 and row<r and col>=0 and col<c:
-    q.append([row,col])
+def appendIf(q, row, col, r, c):
+  if row >= 0 and row < r and col >= 0 and col < c:
+    q.append([row, col])
 
 
-def getOthersInIsland(i,j,r,c,binaryMatrix):
+def getOthersInIsland(i, j, r, c, binaryMatrix):
   q = []
-  q.append([i,j])
-  while len(q)>0:
+  q.append([i, j])
+  while len(q) > 0:
     cell = q.pop(0)
     row = cell[0]
     col = cell[1]
     if binaryMatrix[row][col] == 1:
       binaryMatrix[row][col] = 2
-      appendIf(q,row-1,col,r,c)
-      appendIf(q,row+1,col,r,c)
-      appendIf(q,row,col-1,r,c)
-      appendIf(q,row,col+1,r,c)
+      appendIf(q, row - 1, col, r, c)
+      appendIf(q, row + 1, col, r, c)
+      appendIf(q, row, col - 1, r, c)
+      appendIf(q, row, col + 1, r, c)
+
 
 def get_number_of_islands(binaryMatrix):
   if binaryMatrix == [[]] or binaryMatrix == None:
@@ -25,15 +26,19 @@ def get_number_of_islands(binaryMatrix):
   c = len(binaryMatrix[0])
   for i in xrange(r):
     for j in xrange(c):
-      if binaryMatrix[i][j]==1:
-        islands+=1
-        getOthersInIsland(i,j,r,c,binaryMatrix)
+      if binaryMatrix[i][j] == 1:
+        islands += 1
+        getOthersInIsland(i, j, r, c, binaryMatrix)
   return islands
 
-binaryMatrix = [ [0,    1,    0,    1,    0],
-                         [0,    0,    1,    1,    1],
-                         [1,    0,    0,    1,    0],
-                         [0,    1,    1,    0,    0],
-                         [1,    0,    1,    0,    1] ]
-mat = [[1,1],[1,1]]
+
+binaryMatrix = [[0, 1, 0, 1, 0],
+                [0, 0, 1, 1, 1],
+                [1, 0, 0, 1, 0],
+                [0, 1, 1, 0, 0],
+                [1, 0, 1, 0, 1]]
+mat = [[1, 1],
+       [1, 1]]
+
+print get_number_of_islands(binaryMatrix)
 print get_number_of_islands(mat)
