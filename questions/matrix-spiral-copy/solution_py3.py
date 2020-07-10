2f0316aad95667cc955b15c4cde3376a43b23337
Young <kywbaek@gmail.com>
2020-07-10 09:46:40 -0400

    feat: add solution in python3

---
+++ questions/matrix-spiral-copy/solution_py3.py
@@ -0,0 +1,40 @@
+def spiral_copy(inputMatrix):
+  maxCol = len(inputMatrix[0]) - 1
+  maxRow = len(inputMatrix) - 1
+  minCol = 0
+  minRow = 0
+  result = []
+  while minRow <= maxRow and minCol <= maxCol:
+    for j in range(minCol, maxCol + 1):
+      result.append(inputMatrix[minRow][j])
+    minRow += 1
+
+    for i in range(minRow, maxRow + 1):
+      result.append(inputMatrix[i][maxCol])
+    maxCol -= 1
+
+    if minRow <= maxRow:
+      for j in range(maxCol, minCol - 1, -1):
+        result.append(inputMatrix[maxRow][j])
+      maxRow -= 1
+
+    if minCol <= maxCol:
+      for i in range(maxRow, minRow - 1, -1):
+        result.append(inputMatrix[i][minCol])
+      minCol += 1
+
+  return result
+
+
+print(spiral_copy([[1, 2, 3, 4, 5],
+                   [6, 7, 8, 9, 10],
+                   [11, 12, 13, 14, 15],
+                   [16, 17, 18, 19, 20]]))
+'''
+[[1]]
+[[1,2],[3,4]]
+[[1],[2]]
+[[1,2]]
+[[1,2,3,4,5],[6,7,8,9,10]]
+[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
+'''
