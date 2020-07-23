dc95182b39fb07e9b1e7fb1e81c3c2fe083d0bd4
Young <kywbaek@gmail.com>
2020-07-23 11:21:26 -0400

    feat: add an example case

--- questions/merging-2-packages/solution.py
+++ questions/merging-2-packages/solution.py
@@ -6,3 +6,10 @@ def get_indices_of_item_wights(arr, limit):
         else:
             dic[arr[i]] = i
     return []
+
+
+arr = [4, 6, 10, 15, 16]
+lim = 21
+
+# Expected output: [3, 1]
+print(get_indices_of_item_wights(arr, lim))
